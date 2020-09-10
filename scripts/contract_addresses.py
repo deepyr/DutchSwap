c6f6de10d302ec08c451ff5cbe1774b908f173ba
Adrian Guerrera <adrianguerrera@gmail.com>
2020-09-10 03:37:23 +0200

    Updated deployment scripts

---
+++ scripts/contract_addresses.py
@@ -0,0 +1,24 @@
+
+CONTRACTS = {
+    "ropsten": {
+        "token_factory": "0xDAD930b252bcd95fA5bfaeEa20420283DbfBc94c",
+        "auction_factory":"0x1D0FF5a10E07b4D4e456035149A72e8CcBd855E0"
+
+    }, 
+    "mainnet": {
+        "token_factory": "0xA550114ee3688601006b8b9f25e64732eF774934"
+    },
+    "rinkeby": {
+        "token_factory": "0x1C3e1D406E64004416Fd592C55f9eDeD1A76Bae8",
+        "auction_factory":"0x484af7e370d11151B3CeE3552da74cE9249ea01C"
+    }, 
+    "kovan": {
+        "token_factory": "0x1C3e1D406E64004416Fd592C55f9eDeD1A76Bae8",
+        "auction_factory":"0x484af7e370d11151B3CeE3552da74cE9249ea01C"
+    }, 
+    "goerli": {
+        "token_factory": "0x1C3e1D406E64004416Fd592C55f9eDeD1A76Bae8",
+        "auction_factory":"0x484af7e370d11151B3CeE3552da74cE9249ea01C"
+    }
+}
+
